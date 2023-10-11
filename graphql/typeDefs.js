9bed6039ca1bc46fa740e0a6c11dee17d7383668
Siyadkp <siyadkottakkal@gmail.com>
2023-10-11 08:04:18 -0700

    Added basic functions of graphql

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,10 +1,27 @@
-const {gql} = require('apollo-server');
+const { gql } = require('apollo-server');
 
-module.exports=gql`
-type Recipe {
-    name:String,
-    description:String,
-    createdAt:String,
-    thumbsUp:Int,
-    thambsDown:Int
-}`;
+module.exports = gql`
+  type Recipe {
+    name: String
+    description: String
+    createdAt: String
+    thumbsUp: Int
+    thumbsDown: Int
+  }
+
+  input RecipeInputs {
+    name:String
+    description: String
+  }
+
+  type Query {
+    recipe(ID:ID!):Recipe!
+    getRecipes(amount:Int): [Recipe]
+  }
+
+  type Mutation {
+    createRecipe(recipeInputs:RecipeInputs):Recipe!
+    deleteRecipe(ID:ID!) : Boolean
+    editRecipe(ID:ID!,recipeInput : RecipeInputs):Boolean
+  }
+`;
